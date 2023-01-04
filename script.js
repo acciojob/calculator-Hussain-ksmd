68f5e455009b4a558bcd9c6da891586cb58da43d
Mohammad Hussain <95843547+Hussain-ksmd@users.noreply.github.com>
2023-01-04 09:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 //your code here
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll("button");
+
+let operand1 = null;
+let operand2 = null;
+let operator = null;
+let result = null;
+
+for (let button of buttons) {
+  button.addEventListener("click", function() {
+    const value = this.innerText;
+    if (value === "C") {
+      input.value = "0";
+      operand1 = null;
+      operand2 = null;
+      operator = null;
+      result = null;
+    } else if (value === "+" || value === "-" || value === "*" || value === "/") {
+      operator = value;
+      operand1 = input.value;
+      input.value = "0";
+    } else if (value === ".") {
+      if (!input.value.includes(".")) {
+        input.value += ".";
+      }
+    } else if (value === "=") {
+      operand2 = input.value;
+      if (operand1 !== null && operand2 !== null && operator !== null) {
+        result = eval(operand1 + operator + operand2);
+        if (result === Infinity) {
+          input.value = "Cannot divide by 0";
+        } else if (isNaN(result)) {
+          input.value = "0/0 is undefined";
+        } else {
+          input.value = result;
+        }
+        operand1 = null;
+        operand2 = null;
+        operator = null;
+        result = null;
+      }
+    } else {
+      if (input.value === "0") {
+        input.value = value;
+      } else {
+        input.value += value;
+      }
+    }
+  });
+}
